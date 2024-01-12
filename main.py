44d6ed1d5ce88604d2c225e861c9419609eb350e
nathancurameng <nathan.curameng@gmail.com>
2024-01-12 18:12:31 -0500

    Refactored for readability, added comments and changed variabled names

--- main.py
+++ main.py
@@ -1,35 +1,90 @@
-import assemblyai as aai
-# import pyttsx3
-import praw
-from moviepy.editor import *
-import pysrt
-from ibm_watson import TextToSpeechV1
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import creds
+#~~~~~~~~~~~~~ IMPORTS ~~~~~~~~~~~~~#
+import assemblyai as aai #Imports Assembly AI to convert Speech to Subtitle file
+import praw #Imports praw reddit web scraper
+from moviepy.editor import * #imports moviepy video file editor
+import pysrt #import pysrt to help work with SRT files
+from ibm_watson import TextToSpeechV1 #import IMB Watson to convert text to speech
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator #import watson auth method
+import creds #import API keys, file is ignored
 
-WATSONURL = creds.WATSONURLCRED #DONT CHANGE
+
+
+#~~~~~~~~~~~~~ USER INPUTS ~~~~~~~~~~~~~#
+#THESE ARE API KEYS, YOU CAN CHANGE THEM TO YOUR INFORMATION
+
+#From IMB Waton Text to speech solution
+WATSONURL = creds.WATSONURLCRED 
 WATSONAPIKEY =  creds.WATSONAPIKEYCRED#make this your watson api
+
+#From Assembly AI
 aai.settings.api_key = creds.AAISETTINGS
+
+#From Reddit
 REDDITCLIENTID = creds.REDDITCLIENTIDCRED   #make this your reddit client
 REDDITCLIENTSECRET = creds.REDDITCLIENTSECRETCRED #make this your reddit secret
+NUMBEROFPOSTS = 1 #number of posts dont recommend higher than 3 due to IMB limit
+SUBREDDITNAME = "AmItheAsshole" #subreddit to take from
+
+
+#Add an mp4 to the video path and change this to the file name
 VIDEOFILE = "" #make this your video file path
-NUMBEROFPOSTS = 1
 
-def clipMasterVideo(listSize, timeArray):
-    for x in range(listSize):
-        startTime = int(timeArray[x][0])
-        endTime = int(timeArray[x][1])
-        current_video = VideoFileClip("mastervideo.mp4").subclip(startTime, endTime).without_audio()
-        current_audio = AudioFileClip("audio" + str(x) + ".mp3")
-        new_audioclip = CompositeAudioClip([current_audio])
-        current_video.audio = new_audioclip
-        current_video.write_videofile(("Video" + str(x) + ".mp4"), fps= 25)
 
-def getTimes(listSize):
+#~~~~~~~~~~~~~ METHODS ~~~~~~~~~~~~~#
+#This method scrapes reddit using Praw and users reddit API ID's and returns a list of strings which are the stories
+def scrape_reddit():
+    storyList = []
+    story = ""
+    reddit = praw.Reddit(
+    client_id= REDDITCLIENTID,
+    client_secret= REDDITCLIENTSECRET,
+    user_agent="Reddit Scraper by smeech",
+)
+    #Takes info from inputs to parse through number of stories on the Reddit object created by praw, removing new lines and extra chars then adding it to a list and returning
+    subreddit_name = SUBREDDITNAME
+    num_posts_to_retrieve = NUMBEROFPOSTS
+    subreddit = reddit.subreddit(subreddit_name)
+    top_posts = subreddit.new(limit=num_posts_to_retrieve)
+    for post in top_posts:
+        story = (post.title + post.selftext)
+        story = [line.replace('\n','') for line in story]
+        story = [line.replace("\\",'') for line in story]
+        story = ''.join(str(line) for line in story)
+        storyList.append(story)
+    return(storyList)
+
+#Takes a list of strings as an input and returns 
+def generate_text_to_speech_mp3(listOfStories):
+    #counts the number of stories generated
+    count = 0
+    #Authenticates Watson user and creates ttsObject
+    authenticatior = IAMAuthenticator(WATSONAPIKEY)
+    ttsObject = TextToSpeechV1(authenticator=authenticatior)
+    ttsObject.set_service_url(WATSONURL)
+    #go through the list of stories and generate mp3 audio files based on the strings.
+    for x in range(len(listOfStories)):
+        with open ("./audio" + str(x)+ ".mp3", "wb") as audio_file:
+            res = ttsObject.synthesize(listOfStories[x], accept="audio/mp3", voice = 'en-US_AllisonExpressive').get_result()
+            audio_file.write(res.content)
+        #Add to count and return
+        count += 1
+    return count
+
+#This function generates SRT files using the MP3's generated from generate_text_to_speech_mp3 and Assembly AI's speech to SRT function 
+def generate_srt(numStories):
+    for x in range(0, numStories):
+        transcript = aai.Transcriber().transcribe("audio" + str(x) + ".mp3")
+        subtitlesTranscript = transcript.export_subtitles_srt()
+        f = open("subtitles" + str(x) + ".srt","a")
+        f.write(subtitlesTranscript)
+        f.close()
+
+#This function takes the number of stories and generates an array of ints that correspond to the amount of time the mp3 file is
+def get_times(numStories):
     l = []
     totalSeconds = 0
     audioDuration = 0
-    for x in range(listSize):
+    for x in range(numStories):
         audioclip = AudioFileClip("audio"  + str(x) + ".mp3")
         audioDuration = audioclip.duration
         if(totalSeconds == 0):
@@ -40,67 +95,27 @@ def getTimes(listSize):
             totalSeconds += audioDuration
     return(l)
 
-def transcriberTool(listSize):
-    for x in range(0, listSize):
-        transcript = aai.Transcriber().transcribe("audio" + str(x) + ".mp3")
-        subtitlesTranscript = transcript.export_subtitles_srt()
-        f = open("subtitles" + str(x) + ".srt","a")
-        f.write(subtitlesTranscript)
-        f.close()
+#This method takes the VIDEOFILE and takes the number of posts as videoQuanitityArray, and a videoLengthArray holding the length of each MP3 file
+def create_smaller_clips(videoQuanitityArray, videoLengthArray):
+    for x in range(videoQuanitityArray):
+        startTime = int(videoLengthArray[x][0])
+        endTime = int(videoLengthArray[x][1])
+        #Clips master video into the specified lengths based on the getTimes Function
+        #Generates new mp4 clip without audio
+        current_video = VideoFileClip(VIDEOFILE).subclip(startTime, endTime).without_audio()
+        #Takes audio that corresponds with mp4 clip
+        current_audio = AudioFileClip("audio" + str(x) + ".mp3")
+        new_audioclip = CompositeAudioClip([current_audio])
+        #Adds audio to new video clip
+        current_video.audio = new_audioclip
+        #Generate MP4 file with cut MP4 and AI generated text to speech mp3.
+        current_video.write_videofile(("Video" + str(x) + ".mp4"), fps= 25)
 
-
-def redditScrape():
-    storyList = []
-    story = ""
-    reddit = praw.Reddit(
-    client_id= REDDITCLIENTID,
-    client_secret= REDDITCLIENTSECRET,
-    user_agent="Reddit Scraper by smeech",
-)
-    subreddit_name = "AmItheAsshole"
-    num_posts_to_retrieve = NUMBEROFPOSTS
-    subreddit = reddit.subreddit(subreddit_name)
-    top_posts = subreddit.new(limit=num_posts_to_retrieve)
-    for post in top_posts:
-        story = (post.title + post.selftext)
-        story = [line.replace('\n','') for line in story]
-        story = [line.replace("\\",'') for line in story]
-
-        story = ''.join(str(line) for line in story)
-
-        storyList.append(story)
-
-    return(storyList)
-
-# def tts(message):
-#     count = 0
-#     engine = pyttsx3.init()
-#     voices = engine.getProperty('voices') 
-#     engine.setProperty('voice', voices[2].id)
-#     # engine.say(message)    
-#     for x in range(len(message)):
-#         engine.save_to_file(message[x], "redditVoice" + str(x) + ".mp4", name= None)
-#         engine.runAndWait()
-#         count += 1
-#     return count
-
-def newtts(message):
-    count = 0
-    authenticatior = IAMAuthenticator(WATSONAPIKEY)
-    newttsObj = TextToSpeechV1(authenticator=authenticatior)
-    newttsObj.set_service_url(WATSONURL)
-    for x in range(len(message)):
-        with open ("./audio" + str(x)+ ".mp3", "wb") as audio_file:
-            res = newttsObj.synthesize(message[x], accept="audio/mp3", voice = 'en-US_AllisonExpressive').get_result()
-            audio_file.write(res.content)
-        count += 1
-    return count
-
-
-
+#Takes a time and returns it in seconds to help with the get_times function
 def time_to_seconds(time_obj):
     return time_obj.hours * 3600 + time_obj.minutes * 60 + time_obj.seconds + time_obj.milliseconds / 1000
 
+#Creates the subtitle as formatted by moviepy to place on the video using built in moviepy functions
 def create_subtitles_clips(subtitles, videosize, fontsize = 24, font = "Arial", color = "white", debug = False):
     subtitle_clips = []
     for subtitle in subtitles:
@@ -115,21 +130,28 @@ def create_subtitles_clips(subtitles, videosize, fontsize = 24, font = "Arial", 
         subtitle_clips.append(text_clip.set_position(text_position))
     return subtitle_clips
 
+#Main to run program
 def main():
-    # print(redditScrape())
-    count = newtts(redditScrape())
-    transcriberTool(count)
-    clipMasterVideo(count, getTimes(count))
+    #generates an int returned from generate_text_to_speech_mp3 which is the number of stories, as well as mp3's generated from scrape reddit
+    count = generate_text_to_speech_mp3(scrape_reddit())
+    #Generates SRT's based on the files in the path as well as the number of stories
+    generate_srt(count)
+    #Clips the video based on the count and get_times array
+    create_smaller_clips(count, get_times(count))
+    #Loops through number of posts to create each video
     for x in range(NUMBEROFPOSTS):
+        #Accesses SRT file and mp4 file, and adds _subtitled to the file name
         current_subtitle = pysrt.open("subtitles" + str(x) + ".srt")
         current_video = VideoFileClip("Video"  + str(x) + ".mp4")
         begin, end = ("Video"  + str(x) + ".mp4").split(".mp4")
         outputVideoFile = begin + '_subtitled'+".mp4"
         print ("Output File Name : ", outputVideoFile)
+        #Creates the proper functioning subtitle clip
         subtitle_clips = create_subtitles_clips(current_subtitle, current_video.size)
+        #Creates a final video adding the current video and subtitle clip
         final_video = CompositeVideoClip([current_video] + subtitle_clips)
+        #Writes the final mp4 video with Video, Subtitles and Audio
         final_video.write_videofile(outputVideoFile)
-    #     #add the audio back
 
 if __name__ == '__main__':
     main()
